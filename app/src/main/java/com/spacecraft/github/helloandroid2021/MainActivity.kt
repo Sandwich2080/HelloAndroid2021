a9e5f15b20fa43429e3fd759e5618c2f3a2f70f6
Sandwich2080 <androidsandwich@163.com>
2021-06-21 10:49:54 +0800

    [Added] Create sample for dataBinding usage

--- app/src/main/java/com/spacecraft/github/helloandroid2021/MainActivity.kt
+++ app/src/main/java/com/spacecraft/github/helloandroid2021/MainActivity.kt
@@ -2,10 +2,37 @@ package com.spacecraft.github.helloandroid2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.spacecraft.github.helloandroid2021.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.btnLogin.setOnClickListener {
+            updateTextContent()
+        }
+    }
+
+    private fun updateTextContent() {
+        binding.apply {
+            tvWelcome.text = randomText()
+
+        }
+    }
+
+    private fun randomText():String{
+        var texts = arrayOf("one","two","three","four","five","six","seven","eight","nine","ten")
+        var random = Random()
+        val index = random.nextInt(10)
+        Log.d("MainActivity","index:$index")
+        return texts[index]
     }
 }
+
